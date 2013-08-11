dc764b88d10ffd2aca4a07b9d54e701552be7e4d
Jonathan Camp <jonathan@irondojo.com>
2013-08-11 12:45:41 +0200

    Update rpm.rb

--- attributes/rpm.rb
+++ attributes/rpm.rb
@@ -1,2 +1,2 @@
-default.elasticsearch[:rpm_url] = "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.2.noarch.rpm"
-default.elasticsearch[:rpm_sha] = "a56eb8a88a15adad9ab8e59168c6cd51a1bd9ba3"
+default.elasticsearch[:rpm_url] = "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.3.noarch.rpm"
+default.elasticsearch[:rpm_sha] = "e3fba8901f24546d4a2d2b21872092c3702f2c10"
